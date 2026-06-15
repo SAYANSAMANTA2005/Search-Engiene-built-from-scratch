57cfea75d34f94fd3130da217558327c3c999d44
SAYANSAMANTA2005 <sayansamanta2005@users.noreply.github.com>
2026-06-16 05:20:48 +0530

    chore: remove unused binaries and database file, update Disk_Explorer

--- Explorer/Disk_Explorer.cpp
+++ Explorer/Disk_Explorer.cpp
@@ -58,3 +58,27 @@ void Disk_Explorer(string root, sqlite3* db){
 
     
 }
+
+/*
+    // Replace with your target disk/path
+    fs::path diskPath = "F:\\"; 
+    cout<<"HGhfhfhfh"<<endl;
+      
+       try {
+        if (fs::exists(diskPath) && fs::is_directory(diskPath)) {
+            std::cout << "Iterating top-level of: " << diskPath << "\n\n";
+            
+            for (const auto& entry : fs::directory_iterator(diskPath)) {
+                // entry.path() gives the full path
+                std::cout << entry.path().string() << "\n";
+            }
+        } else {
+            std::cerr << "Path does not exist or is not a directory.\n";
+        }
+    } catch (const fs::filesystem_error& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+
+
+*/
